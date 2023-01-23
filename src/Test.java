eabbe8d41c63b0f2f9ffaf3047e755c7f6182f02
Sami Uysal <samiuysal.business@gmail.com>
2023-01-23 16:00:14 +0300

    Updated

---
+++ src/Test.java
@@ -0,0 +1,14 @@
+
+public class Test {
+    
+    public static void main(String[] args) {
+        
+        
+        
+        Yonetici yonetici1 = new Yonetici("Sami Uysal", 2500, "IT",10);
+        yonetici1.bilgilerigoster();
+        
+        
+    }
+    
+}
